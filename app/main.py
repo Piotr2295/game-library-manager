ea618d811ff85f1242ce437c54cebeda89ecd44e
Piotr Ciszek <pc.ciszekpiotr@gmail.com>
2024-03-07 18:22:23 +0100

    Add swagger docs

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from starlette.responses import HTMLResponse
 
 from app.routers.auth import router as login_router
 from app.routers.registration import router as registration_router
 from app.routers.games import router as games_router
 
-
 app = FastAPI()
 app.include_router(registration_router)
 app.include_router(games_router)
 app.include_router(login_router)
+
+
+@app.get("/docs", response_class=HTMLResponse)
+async def get_swagger_ui():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="Swagger UI")
 
 
 @app.get("/")
